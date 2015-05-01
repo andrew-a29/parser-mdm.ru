70a5d8874c097cec31ca774f75565768433471e5
andrew-a29 <andrew.a29@mail.ru>
2015-05-01 18:03:58 +0300

    Project Update

---
+++ config/console.php
@@ -0,0 +1,11 @@
+<?php
+Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
+
+return [
+    'id' => 'app-console',
+    'bootstrap' => ['gii'],
+    'controllerNamespace' => 'app\commands',
+    'modules' => [
+        'gii' => 'yii\gii\Module',
+    ],
+];
